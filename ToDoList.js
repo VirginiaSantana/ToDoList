5bdf5159d5b49fab2a4a07c910ddf4e6d14b288a
VirginiaSantana <virsantana@gmail.com>
2023-08-21 12:56:45 +0200

    Agregados cambios rama dev

---
+++ ToDoList.js
@@ -0,0 +1,13 @@
+//axios
+//Fetch API
+
+/* async function getBooks(){
+    let result = await fetch("http://localhost:3000/books");
+    let data = await result.json();
+    console.log(data)
+}
+    async function deleteBooks(){
+    let result = await fetch("http://localhost:3000/books", {method: "DELETE"});
+    let data = await result.json();
+    console.log(data)
+} */
